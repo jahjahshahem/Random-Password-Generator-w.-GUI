12c0817c1220be0adc3115cc580489d13df2abbd
Shahem Jahjah <shahemjahjah@gmail.com>
2025-12-18 12:36:07 +0100

    adding logic

---
+++ java/index.js
@@ -0,0 +1,11 @@
+    const length = Number(document.getElementById('passwordLength').value)
+    const includeLower = document.getElementById('lowerCase').checked; 
+    const includeUpper = document.getElementById('upperCase').checked; 
+    const includeNumb = document.getElementById('numbers').checked; 
+    const includeSymb = document.getElementById('symbols').checked; 
+
+function generatePassword(length, includeUpper, includeLower, includeNumb, includeSymb) {
+    
+    console.log(includeLower);
+    
+}
